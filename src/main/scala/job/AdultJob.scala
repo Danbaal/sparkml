a59babc8856c8b02dcf19710cab6aab934ceb99a
Danbaal <lopez.urbano@gmx.com>
2016-05-01 18:35:40 +0200

    Some refactor

--- src/main/scala/job/AdultJob.scala
+++ src/main/scala/job/AdultJob.scala
@@ -1,11 +1,14 @@
 package job
 
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
-import org.apache.spark.ml.feature.{OneHotEncoder, VectorAssembler, StringIndexer}
+import org.apache.spark.ml.classification.{DecisionTreeClassifier, BinaryLogisticRegressionSummary, LogisticRegression}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{VectorIndexer, OneHotEncoder, VectorAssembler, StringIndexer}
+import org.apache.spark.ml.regression.DecisionTreeRegressor
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import transform.Featurizer
 import util.DFCustomFunctions._
 import util.Schemas
 
@@ -26,30 +29,9 @@ object AdultJob extends SparkApp {
 
     val (trainDF, testDF) = loadData(sqlc)
 
-    val label = "income"
-    val indxSuff = "_idx"
-    val vecSuff = "_vec"
+    val (featTrainDF, featTestDF) = Featurizer.transform(trainDF, testDF, label = "income")
 
-    val categoricals: Seq[String] = sch.flatMap(sf => if(sf.dataType == StringType) Some(sf.name) else None)
-    val doubles: Seq[String] = sch.flatMap(sf => if(sf.dataType == DoubleType) Some(sf.name) else None)
-
-    val indexers: Seq[StringIndexer] = categoricals.map(
-      s => new StringIndexer().setInputCol(s).setOutputCol(s+indxSuff)/*.setHandleInvalid("skip")*/)
-    val encoders: Seq[OneHotEncoder] = categoricals.flatMap(
-      s => if(s == label) None else Some(new OneHotEncoder().setInputCol(s+indxSuff).setOutputCol(s+vecSuff)))
-
-    val colsToAssembler = categoricals.flatMap(s => if(s == label) None else Some(s+vecSuff)).++(doubles).toArray
-    val assembler = new VectorAssembler().setInputCols(colsToAssembler).setOutputCol("features")
-
-    val pipeline = new Pipeline().setStages((indexers ++ encoders :+ assembler).toArray)
-    val transformer = pipeline.fit(trainDF)
-    val featTrainDF = transformer.transform(trainDF)
-    val featTestDF = transformer.transform(testDF)
-
-    featTrainDF.printCorrelations(label+indxSuff)
-
-    val featTrainDF_ = featTrainDF.select(col(label+indxSuff).as("label"), col("features"))
-    val featTestDF_ = featTestDF.select(col(label+indxSuff).as("label"), col("features"))
+    ////////////////////////////////////// LOGISTIC REGRESSION //////////////////////////////////////////////
 
     val lr = new LogisticRegression()
     //println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
@@ -57,18 +39,14 @@ object AdultJob extends SparkApp {
       .setRegParam(0.01)
       .setElasticNetParam(0.1)
 
-    val lrModel = lr.fit(featTrainDF_)
+    val lrModel = lr.fit(featTrainDF)
 
-    val result = lrModel.transform(featTestDF_).cache()
+    val lrResult = lrModel.transform(featTestDF).cache()
 
-    val valDF = result.select((col("label") === col("prediction")).as("isAMatch")).cache()
-    val totalRecords = result.count()
-    val matches = valDF.filter(col("isAMatch")).count()
-
-    println("Total records: " + totalRecords)
-    println("Total matches: " + matches)
-    println("Total mistakes: " + (totalRecords - matches))
-
+    val totalRecords = lrResult.count()
+    val lrMatches = lrResult
+      .select((col("label") === col("prediction")).as("isAMatch"))
+      .filter(col("isAMatch")).count()
 
     // Extract the summary from the returned LogisticRegressionModel instance
     val lrSummary = lrModel.summary
@@ -78,19 +56,67 @@ object AdultJob extends SparkApp {
     val binarySummary = lrSummary.asInstanceOf[BinaryLogisticRegressionSummary]
 
     // Obtain the areaUnderROC.
-    val areaUnderROC = binarySummary.areaUnderROC
-    println("Area Under ROC: " + areaUnderROC) // 0.9058589792264902
+    val lrAreaUnderROC = binarySummary.areaUnderROC
 
-    // Set the model threshold to maximize F-Measure
-    /*val fMeasure = binarySummary.fMeasureByThreshold
-    val maxFMeasure = fMeasure.select(max("F-Measure")).head().getDouble(0)
-    val bestThreshold = fMeasure.where($"F-Measure" === maxFMeasure)
-      .select("threshold").head().getDouble(0)
-    lrModel.setThreshold(bestThreshold) ??? */
+    ////////////////////////////////////// DECISION TREE //////////////////////////////////////////////
+
+    // Index labels, adding metadata to the label column.
+    // Fit on whole dataset to include all labels in index.
+    val labelIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      //.fit(featTrainDF_)
+    // Automatically identify categorical features, and index them.
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      //.fit(featTrainDF_)
+
+    // Train a DecisionTree model.
+    val dt = new DecisionTreeClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("indexedFeatures")
+      // Criterion used for information gain calculation. Slightly better result with 'entropy'
+      .setImpurity("entropy")
+      // Minimum information gain for a split to be considered at a tree node.
+      //.setMinInfoGain(0.01)
+
+    val dtPipe = new Pipeline().setStages(Array(labelIndexer, featureIndexer, dt))
+
+    val dtModel = dtPipe.fit(featTrainDF)
+
+    val dtResult = dtModel.transform(featTestDF).cache()
+
+    val matches = dtResult
+      .select((col("label") === col("prediction")).as("isAMatch"))
+      .filter(col("isAMatch")).count()
+
+    val evaluator = new BinaryClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setRawPredictionCol("rawPrediction")
+      //.setMetricName("areaUnderROC") areaUnderROC is by default
+
+    val dtAreaUnderROC = evaluator.evaluate(dtResult)
+
+    /////////////////////////////////// RESULTS ////////////////////////////////////////////////////
+
+    println("\n\nLogistic Regression result:")
+    println("\tTotal records: " + totalRecords) // 15060
+    println("\tTotal matches: " + lrMatches) // 12725
+    println("\tTotal mistakes: " + (totalRecords - lrMatches)) // 2335
+    println("\tArea Under ROC: " + lrAreaUnderROC) // 0.9058589792264902
+
+    println("\nDecision Tree Binary Classifier result:")
+    println("\tTotal records: " + totalRecords) // 15060
+    println("\tTotal matches: " + matches)  // 12356
+    println("\tTotal mistakes: " + (totalRecords - matches)) // 2704
+    println("\tArea Under ROC: " + dtAreaUnderROC) // 0.7820159640274078
+    println()
 
   }
 
   def loadData(sqlc: SQLContext): (DataFrame, DataFrame) = (loadData(sqlc, trainDataPath), loadData(sqlc, testDataPath))
+
   def loadData(sqlc: SQLContext, path: String): DataFrame = sqlc.read
     .format("com.databricks.spark.csv")
     .option("nullValue", " ?")
---
+++ src/main/scala/transform/Featurizer.scala
@@ -0,0 +1,40 @@
+package transform
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.{VectorAssembler, OneHotEncoder, StringIndexer}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DoubleType, StringType}
+
+
+object Featurizer {
+
+  val indxSuff = "_idx"
+  val vecSuff = "_vec"
+
+  def transform(trainDF: DataFrame, testDF: DataFrame, label: String): (DataFrame, DataFrame) = {
+    val sch = trainDF.schema
+
+    val categoricals: Seq[String] = sch.flatMap(sf => if (sf.dataType == StringType) Some(sf.name) else None)
+    val doubles: Seq[String] = sch.flatMap(sf => if (sf.dataType == DoubleType) Some(sf.name) else None)
+
+    val indexers: Seq[StringIndexer] = categoricals.map(
+      s => new StringIndexer().setInputCol(s).setOutputCol(s + indxSuff) /*.setHandleInvalid("skip")*/)
+    val encoders: Seq[OneHotEncoder] = categoricals.flatMap(
+      s => if (s == label) None else Some(new OneHotEncoder().setInputCol(s + indxSuff).setOutputCol(s + vecSuff)))
+
+    val colsToAssembler = categoricals.flatMap(s => if (s == label) None else Some(s + vecSuff)).++(doubles).toArray
+    val assembler = new VectorAssembler().setInputCols(colsToAssembler).setOutputCol("features")
+
+    val pipeline = new Pipeline().setStages((indexers ++ encoders :+ assembler).toArray)
+    val transformer = pipeline.fit(trainDF)
+    val featTrainDF = transformer.transform(trainDF)
+    val featTestDF = transformer.transform(testDF)
+
+    //featTrainDF.printCorrelations(label + indxSuff)
+
+    (featTrainDF.select(col(label + indxSuff).as("label"), col("features")),
+      featTestDF.select(col(label + indxSuff).as("label"), col("features")))
+  }
+
+}
